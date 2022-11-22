b8c98eb58ebc0b06f10a98cf3fe7e92370c894cb
wangyihong1 <118158979+wangyihong1@users.noreply.github.com>
2022-11-22 22:50:55 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -36,7 +36,7 @@ const USER_CONFIG = {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2022', date: '06-05',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
