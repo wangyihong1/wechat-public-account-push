3aa38c93c3c8c833b65be00a1b0f056e1224fade
wangyihong1 <118158979+wangyihong1@users.noreply.github.com>
2022-11-22 22:36:12 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oilh_5y2x7XMHGHU1_JwEu6B2qKk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'HL3MT4XpkVZo8GDEEjPSolLCX3EOl6vDTeMgMQHoIpE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
