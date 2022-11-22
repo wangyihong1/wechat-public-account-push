efe315c76c9f932dc885646c62b072e707fdb99f
wangyihong1 <118158979+wangyihong1@users.noreply.github.com>
2022-11-22 22:52:38 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '红红', year: '2003', date: '05-15',
         },
         {
           type: '节日', name: '恋爱纪念日', year: '2022', date: '06-05',
